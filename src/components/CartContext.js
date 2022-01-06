16c4786dd5ed70c3c68c63ffea20e799ad0fb578
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-01-06 17:27:35 -0300

    Adjust button and style on itemDetail

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,32 +1,32 @@
 import { createContext, useContext } from 'react';
 import { useState } from 'react/cjs/react.development';
 
-const enviro = createContext({ cantidadTotal: 1, cart: []});
+const enviro = createContext({ cantidadTotal: 1, cart: []}); // Example of Context - Not being used.
 
-export const {Provider} = enviro;
+export const {Provider} = enviro; // Example of Provider - We have a custom Provider.
 
 export const useEnviro = () => {  //Custom Hook
     return useContext(enviro)
 }
 
-export const CustomProvider = ({children}) => {
+export const CustomProvider = ({children}) => { // Being used currently as Context.
     const [cartWeight, setCartWeight] = useState(10)
     const [cart, setCart] = useState([])
 
-    const removeItemCart = (id) => {
+    const removeItemCart = (id) => { // Remove item from cart - not cart weight. 
 
     }
-    const emptyCart = () => {
+    const emptyCart = () => { // Empty cart, all items removed. WARNING: CartWeight should also be affected.
         setCart([])
     }
-    const addToCart = (product, amount) => {
+    const addToCart = (product, amount) => { // Element to add must come from a return statement.
         if (isInCart()) {
             
         } else {
             
         }
     }
-    const isInCart = (id) => {
+    const isInCart = (id) => { // Find item inside cart.
 
     }
 
@@ -38,11 +38,11 @@ export const CustomProvider = ({children}) => {
         emptyCart
     }
 
-    return (
+    return ( // We are using a Custom Provider because of the custom hook, but we still need to state Provider.
         <Provider value={contextValues} >
             {children}
         </Provider>
     )
 }
 
-export default CustomProvider;
+export default CustomProvider; // Export this first and foremost as return statement.
