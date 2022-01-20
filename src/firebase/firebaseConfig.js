76f8acbf4efe67e658d64a28daf0a91cf53331f3
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-01-20 20:12:09 -0300

    NoEnv

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY ,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  apiKey: 'AIzaSyC3eOPaITAO3LuYrSL-6egfXeLztYMjna0', //process.env.REACT_APP_FIREBASE_APIKEY ,
+  authDomain: "dnd-ecommerce.firebaseapp.com", //process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
+  projectId: "dnd-ecommerce", //process.env.REACT_APP_FIREBASE_PROJECTID ,
+  storageBucket: "dnd-ecommerce.appspot.com", // process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
+  messagingSenderId: "132953957283", //process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
+  appId: "1:132953957283:web:ef4bf5a566e127f3e53016" //process.env.REACT_APP_FIREBASE_APPID
 };
 
 // Initialize Firebase
